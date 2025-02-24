95119a62ed9b1c111de1e363bee28652b9ec429c
Rafael Silva <rafael.silva@tecnorisk.com.br>
2025-02-24 17:08:12 -0300

    up

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,4 +1,5 @@
 import {NextFunction,Request,Response} from "express"
+import {z} from "zod"
 
 class ProductController{
   async index(request:Request,response:Response,next:NextFunction){
@@ -7,7 +8,24 @@ class ProductController{
     } catch (error) {
       next(error);
     }
+  }
 
+  async create(request:Request,response:Response,next:NextFunction){ 
+    try {
+
+      const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0, {message: "value must be greater than 0"})
+      })
+
+    const {name,price} = bodySchema.parse(request.body)
+
+    return response.status(201).json({name,price})
+      
+    } catch (error) {
+      console.log(error)
+      next(error);
+    }
   }
 }
 
--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -3,4 +3,5 @@ import { Router } from "express"
 const productsRoutes = Router()
 const productController = new ProductController()
 productsRoutes.get("/", productController.index)
+productsRoutes.post("/", productController.create)
 export{productsRoutes}
