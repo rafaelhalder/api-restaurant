d520e5231075074228141a253f87daabc833a150
Rafael Silva <rafael.silva@tecnorisk.com.br>
2025-02-24 16:36:46 -0300

    up

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from "express"
 import { routes } from "./routes"
+import { errorHandling } from "./middlewares/error.handling"
 const PORT = 3333
 const app = express()
 app.use(express.json())
 app.use(routes)
+app.use(errorHandling)
 app.listen(PORT,() => console.log('Server on'))
