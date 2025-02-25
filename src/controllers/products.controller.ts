f37894854f5964b835c857bd07713d5c26313901
Rafael Silva <rafael.silva@tecnorisk.com.br>
2025-02-25 14:31:38 -0300

    up

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,10 +1,15 @@
 import {NextFunction,Request,Response} from "express"
+import {knex} from "@/database/knex"
 import {z} from "zod"
 
 class ProductController{
   async index(request:Request,response:Response,next:NextFunction){
     try {
-      return response.json({message: "ok"})
+
+      const {name} = request.query
+
+      const products = await knex<ProductRepository>('products').select("*").whereLike("name",`%${name ?? ""}%`).orderBy("name")
+      return response.json(products)
     } catch (error) {
       next(error);
     }
@@ -19,14 +24,17 @@ class ProductController{
       })
 
     const {name,price} = bodySchema.parse(request.body)
+    await knex<ProductRepository>('products').insert({name:name,price:price})
 
-    return response.status(201).json({name,price})
+    return response.status(201).json()
       
     } catch (error) {
       console.log(error)
       next(error);
     }
   }
+
+  
 }
 
 export {ProductController}
